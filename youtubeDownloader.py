e11e123ccce2f19dd9e11aad8760c94b29b4be26
Aaron Guerrero <138899922+A-a-rong01@users.noreply.github.com>
2024-07-22 23:39:47 -0700

    Imported tkinter and created the window that pops up when the code is ran

--- youtubeDownloader.py
+++ youtubeDownloader.py
@@ -1,4 +1,26 @@
 import tkinter as tk
-import pytube 
+from tkinter import ttk
 
-print("hello world")
+
+def button_func():
+    print("it works")
+
+#Creating the Window
+window = tk.Tk()
+window.title('Youtube Downloader')
+window.geometry('500x500')
+
+#Creating widgets
+label = ttk.Label(master=window, text='Youtube Downloader', font="Arial 24", padding=5)
+label.pack() 
+
+entry = ttk.Entry(master=window)
+entry.pack()
+
+button = ttk.Button(master=window, text="Download", command=button_func)
+button.pack()
+
+
+
+#The method that runs the App
+window.mainloop()
