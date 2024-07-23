2c1c640f561b82ca3bc9e8018d9a69dc8ab3ebcf
Aaron Guerrero <138899922+A-a-rong01@users.noreply.github.com>
2024-07-23 00:18:39 -0700

    More code finished with the rough frame of the UI now the next step is to integrate pytube and be able to download the video. After I want to add a way to choose your file as well as a progress bar

--- youtubeDownloader.py
+++ youtubeDownloader.py
@@ -1,24 +1,33 @@
 import tkinter as tk
+import customtkinter as ctk
 from tkinter import ttk
+from pytube import YouTube
 
 
-def button_func():
-    print("it works")
+#This is the main function responsible for downloading the video
+def download_video():
+    #grabs the link from the linkEntry widget
+    ytUrl = linkEntry.get()
+    print(ytUrl)
+
+#System Settings
+ctk.set_appearance_mode("system")
 
 #Creating the Window
-window = tk.Tk()
+window = ctk.CTk()
 window.title('Youtube Downloader')
 window.geometry('500x500')
 
-#Creating widgets
+#Creating UI widgets
 label = ttk.Label(master=window, text='Youtube Downloader', font="Arial 24", padding=5)
 label.pack() 
 
-entry = ttk.Entry(master=window)
-entry.pack()
-
-button = ttk.Button(master=window, text="Download", command=button_func)
-button.pack()
+#The entry box where the link is pasted
+linkEntry = ttk.Entry(master=window)
+linkEntry.pack()
+#When the button is clicked the download_video function is called
+download_button = ttk.Button(master=window, text="Download", command=download_video)
+download_button.pack(pady=10)
 
 
 
