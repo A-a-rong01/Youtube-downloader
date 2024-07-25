c64bdfed6f8609bba883d463a09dcc8f80fcdd7b
Aaron Guerrero <138899922+A-a-rong01@users.noreply.github.com>
2024-07-25 00:09:37 -0700

    Bad news is after battling with the script for a while pytube didn't work at all. Good news is though there is another library I can use for this project called yt_dlp. Was able to set that up and actually get the video to download as well as install a button that lets you choose a directory to store it. More work to be done.

--- youtubeDownloader.py
+++ youtubeDownloader.py
@@ -1,14 +1,37 @@
 import tkinter as tk
 import customtkinter as ctk
-from tkinter import ttk
-from pytube import YouTube
+from tkinter import ttk, messagebox, filedialog
+import yt_dlp
+# As of July 24th 2024, Pytube doesn't work so yt_dlp has to be installed to continue project
+# from pytube import YouTube
 
 
+#global variable to store video location
+download_location = ""
 #This is the main function responsible for downloading the video
 def download_video():
-    #grabs the link from the linkEntry widget
-    ytUrl = linkEntry.get()
-    print(ytUrl)
+    try:
+        #grabs the link from the linkEntry widget
+        ytUrl = linkEntry.get()
+        ydl_opts = {
+            'format': 'best',
+            'outtmpl': f'{download_location}/%(title)s.%(ext)s',
+        }
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([ytUrl])
+        messagebox.showinfo("Success", "Download Complete")
+    except Exception as e:
+        messagebox.showerror("Error", f"Youtube link is invalid: {str(e)}")
+    print("Download complete")
+
+    
+def chooseFile():
+    global download_location
+    download_location = filedialog.askdirectory()
+    if download_location:
+        location_label.config(text=f"Download Location: {download_location}")
+    else:
+        location_label.config(text="No location selected")
 
 #System Settings
 ctk.set_appearance_mode("system")
@@ -23,10 +46,15 @@ label = ttk.Label(master=window, text='Youtube Downloader', font="Arial 24", pad
 label.pack() 
 
 #The entry box where the link is pasted
-linkEntry = ttk.Entry(master=window)
+linkEntry = ctk.CTkEntry(master=window)
 linkEntry.pack()
+
+#Button to choose location
+fileLocationButton = ctk.CTkButton(master=window, text="Choose File", command=chooseFile)
+fileLocationButton.pack(pady=5)
+
 #When the button is clicked the download_video function is called
-download_button = ttk.Button(master=window, text="Download", command=download_video)
+download_button = ctk.CTkButton(master=window, text="Download", command=download_video)
 download_button.pack(pady=10)
 
 
